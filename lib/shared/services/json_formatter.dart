a37c6d86235dbc7b4d947eb786e22ef763eb9cc3
amirhossein baghi <dev.amir.b22@gmail.com>
2025-08-09 13:59:20 +0330

    edit json_formatter

--- lib/shared/services/json_formatter.dart
+++ lib/shared/services/json_formatter.dart
@@ -5,12 +5,12 @@ import 'package:nft_market/core/constant.dart';
 import 'package:nft_market/screens/home/data/models/model.dart';
 
 class JsonDataService {
-  Future<List<NFT>> loadHotel() async {
+  Future<List<NFT>> loadNFT() async {
     final String response = await rootBundle.loadString(AppConstant.nftData);
     final Map<String, dynamic> jsonData = json.decode(response);
-    final List<dynamic> hotelsList = jsonData["hotels"];
-    return hotelsList
-        .map((hotel) => NFT.fromJson(hotel as Map<String, dynamic>))
+    final List<dynamic> nftList = jsonData["NFT"];
+    return nftList
+        .map((nft) => NFT.fromJson(nft as Map<String, dynamic>))
         .toList();
   }
 }
